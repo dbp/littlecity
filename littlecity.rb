13357cc3abd0c6156138443b8802aec70572cf5d
Daniel Patterson <dbp@riseup.net>
2011-11-11 22:39:39 -0500

    adding simple form

--- littlecity.rb
+++ littlecity.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
-get '/' do
-  "Hello World!"
+get "/" do
+  "<html><body>
+  <a href='/form'>View form</a>
+  </body></html>"
+end
+
+get '/form' do
+  "<html><body>
+  <form method='post'>
+  <input type='text' name='eggs'/>
+  <input type='submit' value='Submit'/>
+  </form>
+  </body></html>"
+end
+
+post "/form" do
+  "Eggs: #{params[:eggs]}"
 end
