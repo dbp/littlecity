879c14e1a6b9c66311dab2bf9fdc9fca4b1fbb15
Daniel Patterson <dbp@riseup.net>
2011-11-12 14:42:25 -0500

    added peas and made total

--- littlecity.rb
+++ littlecity.rb
@@ -22,7 +22,15 @@ get '/form' do
 end
 
 post "/form" do
-  num_eggs = params[:eggs].to_i
-  cost = 5 # per dozen
-  "Eggs: #{num_eggs} dozen at $#{cost}/dozen = $#{cost * num_eggs}"
+  number_of_eggs = params[:eggs].to_i
+  eggs_price = 5 # per dozen
+  eggs_cost = eggs_price * number_of_eggs
+  
+  number_of_peas = params[:peas].to_i
+  peas_price = 6 # per pound
+  peas_cost = peas_price * number_of_peas
+  
+  "Eggs: #{number_of_eggs} dozen at $#{eggs_price}/dozen = $#{eggs_cost}<br>
+   Peas: #{number_of_peas} pounds at $#{peas_price}/pound = $#{peas_cost}<br>
+   Total: $#{eggs_cost + peas_cost}"
 end
