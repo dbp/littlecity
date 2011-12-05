6142feb7b5a870dd9912e476248db1fb7668fff7
Daniel Patterson <dbp@riseup.net>
2011-12-04 20:21:48 -0500

    adding list, add, remove views, and more actions to make them work!

--- littlecity.rb
+++ littlecity.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'data_mapper'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/littlecity.db")
+# need install dm-sqlite-adapter
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/littlecity.db")
 
-class Item 
+class Item
   include DataMapper::Resource
   
   property :id, Serial
@@ -13,24 +14,54 @@ class Item
   property :price, Float
 end
 
+# automatically create the post table
+Item.auto_migrate! unless Item.storage_exists?
+DataMapper.finalize
+
 get "/" do
   erb :home
 end
 
-get '/form' do
+get "/form" do
   erb :form
 end
 
 post "/form" do
-  number_of_eggs = params[:eggs].to_i
-  eggs_price = 5 # per dozen
-  eggs_cost = eggs_price * number_of_eggs
+  @number_of_eggs = params[:eggs].to_i
+  @eggs_price = 5 # per dozen
+  @eggs_cost = @eggs_price * @number_of_eggs
+    
+  @number_of_peas = params[:peas].to_i
+  @peas_price = 6 # per pound
+  @peas_cost = @peas_price * @number_of_peas
   
-  number_of_peas = params[:peas].to_i
-  peas_price = 6 # per pound
-  peas_cost = peas_price * number_of_peas
-  
-  "Eggs: #{number_of_eggs} dozen at $#{eggs_price}/dozen = $#{eggs_cost}<br>
-   Peas: #{number_of_peas} pounds at $#{peas_price}/pound = $#{peas_cost}<br>
-   Total: $#{eggs_cost + peas_cost}"
+  erb :calculation
+end
+
+get "/list" do
+  @items = Item.all
+  erb :list
+end
+
+get "/add" do
+  @items = Item.all
+  erb :add
+end
+
+post "/add" do
+  Item.create(:name => params["name"], 
+              :units => params["units"], 
+              :quantity => 0, 
+              :price => params["price"].to_f)
+  redirect "/list"
+end
+
+get "/remove" do
+  erb :remove
+end
+
+post "/remove" do
+  item = Item.get(params["id"].to_i)
+  item.destroy
+  redirect "/list"
 end
