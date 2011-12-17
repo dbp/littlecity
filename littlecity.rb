f1b459e396c6b891dfd009e9fd3e18e27b6073dd
katiemiller <johannabloch@Macintosh-5.home>
2011-12-17 18:12:00 -0500

    1

--- littlecity.rb
+++ littlecity.rb
@@ -1,30 +1,4 @@
-require 'sinatra'
-require 'data_mapper'
-
-# need install dm-sqlite-adapter
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/littlecity.db")
-
-class Item
-  include DataMapper::Resource
-  
-  property :id, Serial
-  property :name, String
-  property :quantity, Float
-  property :units, String
-  property :price, Float
-  
-end
-
-# automatically create the post table
-Item.auto_migrate! unless Item.storage_exists?
-DataMapper.finalize
-
-get "/" do
-  erb :home
-end
-
-get "/form" do
-  @items = Item.all
+@items = Item.all
   erb :form
 end
 
@@ -46,10 +20,31 @@ end
 
 get "/list" do
   @items = Item.all
-  item.update(:name => params["name"], 
-              :units => params["units"], 
-              :quantity => params["quantity"], 
-              :price => params["price"].to_f)
+  erb :list
+end
+
+@items = Item.all
+  erb :form
+end
+
+post "/form" do
+  @ordered = []
+  @total_price = 0
+  params.each do |item_id,quantity_ordered|
+    item = Item.get(item_id.to_i)
+    paying = quantity_ordered.to_f * item.price
+    item.update(:quantity => item.quantity - quantity_ordered.to_f)
+    @ordered = @ordered + [{:name => item.name, 
+                            :paying => paying, 
+                            :price => item.price}] unless quantity_ordered.to_f == 0
+    @total_price = @total_price + paying
+  end
+  
+  erb :calculation
+end
+
+get "/list" do
+  @items = Item.all
   erb :list
 end
 
