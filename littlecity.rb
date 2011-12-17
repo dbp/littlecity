6ed95a11530d16a39328ea39a7180832fdaf45ea
Johanna Bloch <johannabloch@macintosh-5.myhome.westell.com>
2011-12-17 13:57:18 -0500

    size box

--- littlecity.rb
+++ littlecity.rb
@@ -60,7 +60,11 @@ end
 
 post "/edit" do
   item = Item.get(params["id"].to_i)
-  item.update(params)
+  #item.update(params)
+  item.update(:name => params["name"], 
+              :units => params["units"], 
+              :quantity => params["quantity"], 
+              :price => params["price"].to_f)
   redirect "/list"
 end
 get "/remove" do
