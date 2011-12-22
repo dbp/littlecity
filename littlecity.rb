bc2e9bc8c0fadf543d449257c42e2e79c5b1aedc
katiemiller <johannabloch@Macintosh-5.local>
2011-12-22 10:01:26 -0500

    edit page change

--- littlecity.rb
+++ littlecity.rb
@@ -65,14 +65,13 @@ post "/add" do
   redirect "/list"
 end
 get "/edit" do
-@item = Item.get(params["id"].to_i)
   erb :edit
 end
 
 post "/edit" do
-  item = Item.get(params["id"].to_i)
+  @items = Item.all
   #item.update(params)
-  item.update(:name => params["name"], 
+  @items.update(:name => params["name"], 
               :units => params["units"], 
               :quantity => params["quantity"], 
               :price => params["price"].to_f)
