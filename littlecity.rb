6734ad2a0cb2f46738d7eff1cadbf4339fe02f85
katiemiller <johannabloch@Macintosh-5.local>
2011-12-22 20:11:03 -0500

    1

--- littlecity.rb
+++ littlecity.rb
@@ -35,7 +35,7 @@ post "/form" do
     item = Item.get(item_id.to_i)
     paying = quantity_ordered.to_f * item.price
    item.update(:quantity => item.quantity - quantity_ordered.to_f) 
-   if quantity < 0, then quantity = sold out
+   when :quantity <= 0 then item.quantity = "sold out"
     @ordered = @ordered + [{:name => item.name, 
                             :paying => paying, 
                             :price => item.price,
